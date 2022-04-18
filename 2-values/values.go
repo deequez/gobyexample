d4d8c7817efc61e3d3deece55617b89878032476
Dee Vazquez <dee.quez11@gmail.com>
2022-04-18 06:09:06 -0700

    2 Values

---
+++ 2-values/values.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Strings can be added together
+	fmt.Println("go" + "lang")
+
+	// Integers & floats
+	fmt.Println("1+1 =", 1+1)
+	fmt.Println("7.0/3.0 =", 7.0/3.0)
+
+	// Booleans with boolean operators
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+}
